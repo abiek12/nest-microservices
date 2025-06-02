a038cdedec1f38265e27acad2d07d83ff900cd69
Abhishek <abhishekkanichery@gmail.com>
2025-06-02 23:08:44 +0530

    feat(db-svc): added db services

---
+++ apps/user-svc/src/db/db.service.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class DbService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Database connected successfully');
+  }
+}
