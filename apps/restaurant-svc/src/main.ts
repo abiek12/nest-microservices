0a340e101bcf5ccc98953050013988856dfd105c
Abhishek <abhishekkanichery@gmail.com>
2025-06-02 08:22:29 +0530

    feat(setup): modified main.ts for microservices

--- apps/restaurant-svc/src/main.ts
+++ apps/restaurant-svc/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: [process.env.KAFKA_BROKER ?? 'localhost:9092'],
+        },
+        consumer: {
+          groupId: process.env.KAFKA_GROUP_ID ?? 'restaurant-consumer',
+        },
+      },
+    },
+  );
+  await app.listen();
+  console.log('Restaurant service is listening...');
+  console.log(`Kafka broker: ${process.env.KAFKA_BROKER ?? 'localhost:9092'}`);
 }
 bootstrap();
