f3d40ce64face1e5b22cab69150c1499fa8a6331
Abhishek <abhishekkanichery@gmail.com>
2025-06-04 08:01:51 +0530

    feat(auth): added auth services in user microservices

--- apps/user-svc/src/main.ts
+++ apps/user-svc/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { randomUUID } from 'crypto';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -9,6 +10,7 @@ async function bootstrap() {
       transport: Transport.KAFKA,
       options: {
         client: {
+          clientId: `user-${randomUUID()}`,
           brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
         },
         consumer: {
